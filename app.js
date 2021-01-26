a4c257d407c80b80acda7f7de97863e34e842fcc
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-26 04:26:39 +0100

    Ejs template pages

--- app.js
+++ app.js
@@ -6,20 +6,22 @@ const app = express();
 // Listen for requests
 app.listen(3000);
 
+// register view engine
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-// Redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });

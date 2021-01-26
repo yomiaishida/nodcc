3c8bffc61560f88951a8a08b9245bc9d11601e75
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-26 02:31:24 +0100

    Settng up an express app

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+// express app
+const app = express();
+
+// Listen for requests
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// Redirects
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});

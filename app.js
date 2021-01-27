478f1281fe2b599df5579ed9ba70b5ae88e248ba
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-27 01:58:21 +0100

    Making post request to add new blog

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-app.use(morgan("dev"));
-
 // middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // register view engine
 app.set("view engine", "ejs");
@@ -43,6 +43,19 @@ app.get("/blogs", (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
     })
     .catch((err) => {
       console.log(err);
